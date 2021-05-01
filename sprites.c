e0d0a54b41a169c25535617169185e7d20e45bea
Marcus <marcusvmelodev@gmail.com>
2021-05-01 00:17:53 -0300

    Improved Spacecraft Struct, Finished InitalizeSpacecraft, Implemented DrawSpacecraft

--- sprites.c
+++ sprites.c
@@ -2,16 +2,30 @@
 
 struct Spacecraft
 {
-    int center[2];
-    int life;
+    Vector2 vertices[3];
+    Vector2 center;
     Color color;
+    int life;
 };
 
-struct Spacecraft InitializeSpacecraft(int center[2], int life, Color color)
+struct Spacecraft 
+InitializeSpacecraft (Vector2 center, int life, Color color)
 {
+    Vector2 vertex1 = {center.x, center.y - 15};
+    Vector2 vertex2 = {center.x - 15, center.y + 15};
+    Vector2 vertex3 = {center.x + 15, center.y + 15};
     struct Spacecraft spacecraft;
-    spacecraft.center[0] = center[0];
-    spacecraft.center[1] = center[1];
+    spacecraft.center = center;
+    spacecraft.vertices[0] = vertex1;
+    spacecraft.vertices[1] = vertex2;
+    spacecraft.vertices[1] = vertex3;
     spacecraft.life = life;
     spacecraft.color = color;
+    return spacecraft;
 };
+
+int 
+DrawSpacecraft (struct Spacecraft *spac)
+{
+
+}
